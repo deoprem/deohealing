02e6f5fecff6dc48ab18747719265de0c34771a9
Deo Prem <deo@DeodeMacBook-Air.local>
2025-06-15 12:15:52 +0800

    更新網站內容

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,19 +4,59 @@ import { getBlogPosts } from '../lib/notion.js'
 export const GET: APIRoute = async () => {
   const posts = await getBlogPosts()
 
-  const urls = posts.map((post) => {
-    return `
+  // 靜態頁面（你可以依網站實際內容修改）
+  const staticPages = [
+    {
+      loc: 'https://deoheal.com/',
+      lastmod: '2025-06-15',
+    },
+    {
+      loc: 'https://deoheal.com/aboutdeo',
+      lastmod: '2025-06-15',
+    },
+    {
+      loc: 'https://deoheal.com/contact',
+      lastmod: '2025-06-15',
+    },
+    {
+      loc: 'https://deoheal.com/services',
+      lastmod: '2025-06-15',
+    },
+    {
+      loc: 'https://deoheal.com/courses/',
+      lastmod: '2025-06-15',
+    },
+    {
+      loc: 'https://deoheal.com/blog/',
+      lastmod: '2025-06-15',
+    },
+  ]
+
+  const staticUrls = staticPages
+    .map(
+      (page) => `
+      <url>
+        <loc>${page.loc}</loc>
+        <lastmod>${page.lastmod}</lastmod>
+      </url>`
+    )
+    .join('')
+
+  const blogUrls = posts
+    .map(
+      (post) => `
       <url>
         <loc>https://deoheal.com/blog/${post.slug}</loc>
         <lastmod>${new Date(post.date).toISOString()}</lastmod>
-      </url>
-    `
-  }).join('')
+      </url>`
+    )
+    .join('')
 
   return new Response(
-    `<?xml version="1.0" encoding="UTF-8" ?>
+    `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${urls}
+      ${staticUrls}
+      ${blogUrls}
     </urlset>`.trim(),
     {
       headers: {
