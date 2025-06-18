4d2669985d07f9df6591625dcfa0e4fa3f364771
Deo Prem <enzowo@gmail.com>
2025-06-18 23:05:35 +0800

    更新網站內容

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -35,7 +35,7 @@ export const GET: APIRoute = async () => {
   const allPages = [...staticPages, ...blogPages]
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
   ${allPages
     .map(
       (page) => `
